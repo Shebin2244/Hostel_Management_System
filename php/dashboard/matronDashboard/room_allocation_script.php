98816321d38a197d30ab24845a7980ed39e8b08a
shebin2244 <shebin2244@gmail.com>
2023-11-19 14:34:43 +0530

    room allocation

--- php/dashboard/matronDashboard/room_allocation_script.php
+++ php/dashboard/matronDashboard/room_allocation_script.php
@@ -1,43 +1,86 @@
 <?php
+// Include your database connection
 include '../../../connection/connection.php';
 
-// Function to get room allocations and student details
-function getRoomAllocations() {
+// Function to get room allocations based on room_id
+function getRoomAllocationsByRoomId($room_id)
+{
     global $conn;
 
-    $query = "SELECT rooms.room_name, hostel_student_list.name, hostel_student_list.degree
-              FROM rooms
-              LEFT JOIN allocations ON rooms.room_id = allocations.room_id
-              LEFT JOIN hostel_student_list ON allocations.admission_no = hostel_student_list.admissionNo
-              GROUP BY rooms.room_name, hostel_student_list.admissionNo
-              ORDER BY rooms.room_id, hostel_student_list.degree";
-    $result = $conn->query($query);
+    // Fetch room details
+    $roomQuery = "SELECT * FROM rooms WHERE room_id = '$room_id'";
+    $roomResult = mysqli_query($conn, $roomQuery);
 
-    $roomAllocations = array();
+    if ($roomResult) {
+        $roomDetails = mysqli_fetch_assoc($roomResult);
 
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $roomName = $row['room_name'];
-            $studentName = $row['name'];
-            $degree = $row['degree'];
+        // Fetch room allocations
+        $allocationQuery = "SELECT hostel_student_list.name, hostel_student_list.admissionNo, hostel_student_list.degree
+                            FROM allocations
+                            JOIN hostel_student_list ON allocations.admission_no = hostel_student_list.admissionNo
+                            WHERE allocations.room_id = '$room_id'";
+        $allocationResult = mysqli_query($conn, $allocationQuery);
 
-            if (!isset($roomAllocations[$roomName])) {
-                $roomAllocations[$roomName] = array();
-            }
+        if ($allocationResult) {
+            $allocations = mysqli_fetch_all($allocationResult, MYSQLI_ASSOC);
 
-            if (!isset($roomAllocations[$roomName][$degree])) {
-                $roomAllocations[$roomName][$degree] = array();
-            }
+            // Organize data
+            $roomAllocations = [
+                'Room Name' => $roomDetails['room_name'],
+                'Capacity' => $roomDetails['capacity'],
+                'Allocations' => $allocations
+            ];
 
-            if (count($roomAllocations[$roomName][$degree]) < 4) {
-                $roomAllocations[$roomName][$degree][] = $studentName;
-            }
+            return $roomAllocations;
+        } else {
+            // Handle the case where fetching allocations fails
+            echo "Error fetching room allocations for Room ID: $room_id. Please try again.";
+            exit();
         }
+    } else {
+        // Handle the case where fetching room details fails
+        echo "Error fetching room details for Room ID: $room_id. Please try again.";
+        exit();
     }
-
-    return $roomAllocations;
 }
 
-// Get and display room allocations
-$roomAllocations = getRoomAllocations();
+// Function to get all room allocations
+function getAllRoomAllocations()
+{
+    global $conn;
+
+    // Fetch all room IDs
+    $roomIdsQuery = "SELECT DISTINCT room_id FROM allocations";
+    $roomIdsResult = mysqli_query($conn, $roomIdsQuery);
+
+    if ($roomIdsResult) {
+        $roomIds = mysqli_fetch_all($roomIdsResult, MYSQLI_ASSOC);
+
+        // Initialize an array to store all room allocations
+        $allRoomAllocations = [];
+
+        foreach ($roomIds as $roomId) {
+            $room_id = $roomId['room_id'];
+
+            // Get allocations for each room
+            $roomAllocations = getRoomAllocationsByRoomId($room_id);
+
+            // Add room allocations to the main array
+            $allRoomAllocations[] = $roomAllocations;
+        }
+
+        return $allRoomAllocations;
+    } else {
+        // Handle the case where fetching room IDs fails
+        echo "Error fetching room IDs. Please try again.";
+        exit();
+    }
+}
+
+// Fetch and return all room allocations
+function getRoomAllocations()
+{
+    // Call the function to get all room allocations
+    return getAllRoomAllocations();
+}
 ?>
--- php/dashboard/matronDashboard/view_rooms.php
+++ php/dashboard/matronDashboard/view_rooms.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, 
-				initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RIT Hostel</title>
     <link rel="stylesheet" href="../../../style/dash-style.css">
     <link rel="stylesheet" href="../../../style/responsive.css">
@@ -48,46 +47,43 @@
         .program {
             font-weight: bold;
         }
-
     </style>
 </head>
 
 <body>
 
-    <!-- for header part -->
-    <header>
+<header>
 
-        <div class="logosec">
-            <div class="logo">Matron Dashboard</div>
-            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210182541/Untitled-design-(30).png"
-                class="icn menuicn" id="menuicn" alt="menu-icon">
-        </div>
+<div class="logosec">
+    <div class="logo">Matron Dashboard</div>
+    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210182541/Untitled-design-(30).png"
+        class="icn menuicn" id="menuicn" alt="menu-icon">
+</div>
 
-        <div class="searchbar">
-            <input type="text" placeholder="Search">
-            <div class="searchbtn">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210180758/Untitled-design-(28).png"
-                    class="icn srchicn" alt="search-icon">
-            </div>
-        </div>
+<div class="searchbar">
+    <input type="text" placeholder="Search">
+    <div class="searchbtn">
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210180758/Untitled-design-(28).png"
+            class="icn srchicn" alt="search-icon">
+    </div>
+</div>
 
-        <div class="message">
-            <div class="circle"></div>
-            <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210183322/8.png" class="icn" alt="">
-            <div class="dp">
-                <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210180014/profile-removebg-preview.png"
-                    class="dpicn" alt="dp">
-            </div>
-        </div>
+<div class="message">
+    <div class="circle"></div>
+    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210183322/8.png" class="icn" alt="">
+    <div class="dp">
+        <img src="https://media.geeksforgeeks.org/wp-content/uploads/20221210180014/profile-removebg-preview.png"
+            class="dpicn" alt="dp">
+    </div>
+</div>
 
-    </header>
+</header>
 
     <div class="main-container">
-    <?php
-       include "../../../component/sidebar/matron.php";
-       ?>
+        <?php
+        include "../../../component/sidebar/matron.php";
+        ?>
         <div class="main">
-
             <div class="searchbar2">
                 <input type="text" name="" id="" placeholder="Search">
                 <div class="searchbtn">
@@ -96,41 +92,40 @@
                 </div>
             </div>
 
+           
     <h1>Room Allocation View</h1>
-    
-    <?php
-    // Include the PHP script here
-    include 'room_allocation_script.php';
-    
-    // Get and display room allocations
-    $roomAllocations = getRoomAllocations();
 
-    echo '<div class="room-container">';
-    foreach ($roomAllocations as $roomName => $allocations) {
-        echo '<div class="room">';
-        echo '<h3>Room: ' . $roomName . '</h3>';
+<?php
+// Include the PHP script here
+include 'room_allocation_script.php';
 
-        echo '<ul class="students">';
-        foreach ($allocations as $program => $students) {
-            echo '<li class="student">';
-            echo '<span class="program">Program: ' . $program . '</span>';
-            echo '<ul>';
-            foreach ($students as $student) {
-                echo '<li>' . $student . '</li>';
-            }
-            echo '</ul>';
-            echo '</li>';
-        }
+// Get and display room allocations
+$roomAllocations = getRoomAllocations();
+
+echo '<div class="room-container">';
+foreach ($roomAllocations as $roomDetails) {
+    echo '<div class="room">';
+    echo '<h3>Room: ' . $roomDetails['Room Name'] . '</h3>';
+    // echo '<p>Capacity: ' . $roomDetails['Capacity'] . '</p>';
+
+    echo '<ul class="students">';
+    foreach ($roomDetails['Allocations'] as $allocation) {
+        echo '<li class="student">';
+        echo '<ul style="background-color:rgb(255,255,255)">';
+        echo '<li>Name: ' . $allocation['name'] . '</li>';
+        echo '<li>Admission No: ' . $allocation['admissionNo'] . '</li>';
+        echo '<li>Branch: ' . $allocation['degree'] . '</li>';
         echo '</ul>';
-
-        echo '</div>';
+        echo '</li>';
     }
+    echo '</ul>';
+
     echo '</div>';
-    ?>
+}
+echo '</div>';
+?>
 
-
-
-    <script src="../../style/dashboard.js"></script>
+            <script src="../../../style/dashboard.js"></script>
 </body>
 
 </html>
